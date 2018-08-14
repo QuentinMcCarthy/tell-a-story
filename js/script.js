e4f45829b4ef8f99e130efac80f7459ae4b6355a
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-15 11:33:13 +1200

    Fixed CSS as per lint; calling data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
+function loadData(){
+	$.ajax({
+		type: "GET",
+		url: "data/surveyData.json",
+		dataType: "json",
+		success: function(data){
+			console.log(data);
 
+
+		},
+		error: function(err){
+			console.log("Error "+err.status);
+			console.log(err);
+		}
+	});
+}
+
+function createVisualizations(){
+
+}
+
+$(document).ready(function(){
+	loadData();
 });
