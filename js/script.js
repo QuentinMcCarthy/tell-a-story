80051645b192a9240e0213708d87df075aa156d5
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-15 11:51:39 +1200

    Removed jQuery knob

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function loadData(){
 		success: function(data){
 			console.log(data);
 
-
+			createvisualizations(data);
 		},
 		error: function(err){
 			console.log("Error "+err.status);
@@ -16,7 +16,25 @@ function loadData(){
 }
 
 function createVisualizations(){
-
+	// $("#knobVisualization").knob({
+	// 	"min": 0,
+	// 	"max": 100,
+	// 	"step": 1,
+	// 	"angleOffset": 0,
+	// 	"angleArc": 360,
+	// 	"stopper": true,
+	// 	"readOnly": true,
+	// 	"rotation": clockwise,
+	// 	"cursor": false,
+	// 	"thickness": .35,
+	// 	"displayInput": true,
+	// 	"displayPrevious": false,
+	// 	"release":function(){},
+	// 	"change":function(){},
+	// 	"draw":function(){},
+	// 	"cancel":function(){},
+	// 	"format":function(){}
+	// });
 }
 
 $(document).ready(function(){
