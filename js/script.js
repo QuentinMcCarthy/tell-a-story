9718e18468706c4e0601263b510e21531f9a67bc
Jiah <itisgia@gmail.com>
2018-08-17 11:53:07 +1200

    pie and styling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-google.charts.load('current', {'packages':['bar']});
+google.charts.load('current', {'packages':['corechart','bar']});
 google.charts.setOnLoadCallback(loadData);
 var array = [];
 function loadData(){
@@ -8,7 +8,8 @@ function loadData(){
 		dataType: "json",
 		success: function(dataFromSurvey){
 			transportation(dataFromSurvey.totals.transportation);
-			drawBar(dataFromSurvey.totals.gender);
+			drawBar(dataFromSurvey.totals.beforeYoobee);
+			donutChart(dataFromSurvey.totals.currentCourse);
 
 		},
 		error: function(err){
@@ -68,25 +69,53 @@ function drawBar(current) {
 
 	dataGender.addColumn('string', 'Gender');
 	dataGender.addColumn('number', 'Amount');
-	array[0].data.forEach(function(currentValue,index){
-		for (var i = 0; i < array[0].data.length; i++) {
-				dataGender.addRow([
-					 array[0].titles[i],array[0].data[i],
-				]);
-			}
-});
-
-
+	// array[0].data.forEach(function(currentValue,index){
+	for (var i = 0; i < array[0].data.length; i++) {
+			dataGender.addRow([
+				array[0].titles[i],array[0].data[i],
+			]);
+	};
+	// });
 	var options = {
-		title: 'Male & Female',
+		title: 'Before Yoobee',
 		bars: 'horizontal',
-		// width: 400,
-		vAxis: {minValue: 0}
-		// backgroundColor: ''
+		colors:['#899ebf'],
+		vAxis: {
+			minValue: 0
+		},
+
+		backgroundColor: {
+					fill: 'transparent'
+		}
   	};
 
   var chart = new google.charts.Bar(document.getElementById('barChart'));
   chart.draw(dataGender, options);
+}
+
+//donut Chart
+function donutChart(courses) {
+	array.push(courses);
+	console.log(array);
+	var dataCourse = new google.visualization.DataTable();
+	dataCourse.addColumn('string', 'Courses');
+	dataCourse.addColumn('number','Amount');
+	for (var i = 0; i < array[1].data.length; i++) {
+			dataCourse.addRow([
+				array[1].titles[i],array[1].data[i],
+			]);
+			console.log(array[1].titles[i]);
+			console.log(array[1].data[i]);
+	};
+	var options = {
+		// pieHole:0.3,
+		colors: ['#d4d0db', '#ccbae2', '#ae9bc6','#a089bc','#836aa3','#664d87','#34165b','#470a96']
+	};
+
+	var chart = new google.visualization.PieChart(document.getElementById('donuty'));
+	// var chart = new google.visualization.PieChart(document.getElementById('donuty'));
+	chart.draw(dataCourse, options);
+
 }
 
 
