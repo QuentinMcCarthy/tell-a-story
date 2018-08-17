7df645fa1f934a42ce47e5af979b90abe4d78963
jiah kwon <itisgia@gmail.com>
2018-08-17 16:52:23 +1200

    geo chart has added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-google.charts.load('current', {'packages':['corechart','bar']});
+google.charts.load('current', {'packages':['corechart','bar','geochart'],'mapsApiKey': 'AIzaSyCGWQknqUrD9uCL0vx-Mv5Fra41NFaHERU'});
 google.charts.setOnLoadCallback(loadData);
 var array = [];
 function loadData(){
@@ -6,10 +6,11 @@ function loadData(){
 		type: "GET",
 		url: "data/surveyData.json",
 		dataType: "json",
-		success: function(dataFromSurvey){
-			transportation(dataFromSurvey.totals.transportation);
-			drawBar(dataFromSurvey.totals.beforeYoobee);
-			donutChart(dataFromSurvey.totals.currentCourse);
+		success: function(data){
+			transportation(data.totals.transportation);
+			drawBar(data.totals.beforeYoobee);
+			donutChart(data.totals.currentCourse);
+			geoChart(data.totals.suburb)
 
 		},
 		error: function(err){
@@ -96,7 +97,6 @@ function drawBar(current) {
 //donut Chart
 function donutChart(courses) {
 	array.push(courses);
-	console.log(array);
 	var dataCourse = new google.visualization.DataTable();
 	dataCourse.addColumn('string', 'Courses');
 	dataCourse.addColumn('number','Amount');
@@ -104,10 +104,9 @@ function donutChart(courses) {
 			dataCourse.addRow([
 				array[1].titles[i],array[1].data[i],
 			]);
-			console.log(array[1].titles[i]);
-			console.log(array[1].data[i]);
 	};
 	var options = {
+		title: 'Current courses Yoobee students taking',
 		pieHole:0.3,
 		colors: ['#d4d0db', '#ccbae2', '#ae9bc6','#a089bc','#836aa3','#664d87','#34165b','#470a96']
 	};
@@ -117,6 +116,30 @@ function donutChart(courses) {
 
 }
 
+function geoChart(location) {
+	array.push(location);
+	var dataSuburb  = new google.visualization.DataTable();
+	dataSuburb.addColumn('string','Suburb');
+	dataSuburb.addColumn('number','Amount');
+	// dataSuburb.addColumn('number','lat');
+	// dataSuburb.addColumn('number','lng');
+	console.log(array);
+	for (var i = 0; i < array[2].data.length; i++) {
+			dataSuburb.addRow([
+				array[2].titles[i], array[2].data[i]
+			]);
+	};
+	var options = {
+		region:'NZ',
+		resolution:'provinces',
+		displayMode: 'markers',
+		tooltip: {textStyle: {color: '#444444'}}
+};
+
+
+	var chart = new google.visualization.GeoChart(document.getElementById('geo'));
+ 	chart.draw(dataSuburb, options);
+}
 
 //
 // $(document).ready(function(){
